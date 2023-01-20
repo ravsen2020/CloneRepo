4032b78c0a21df72ba91bb53edcd435af1b6f1f8
ravsen2020 <69644186+ravsen2020@users.noreply.github.com>
2023-01-20 10:09:39 -0500

    first file added

---
+++ CronJobPgm.java
@@ -0,0 +1,10 @@
+public class CronJobPgm {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Vetrivel,   Veeravel");
+		System.out.println("Cron job project");
+
+	}
+
+}
